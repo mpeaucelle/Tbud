070a0a36aebf11738f969ac9d6a0a9f9a5d16f76
mpeaucelle <marc.peaucelle@gmail.com>
2020-07-01 13:32:07 +0200

    Description added

--- R_src/Tbud.R
+++ R_src/Tbud.R
@@ -1,3 +1,22 @@
+############################################################
+# Tbud function
+# Author: Marc Peaucelle, 2020/06
+# Description:
+# This function search for bud temperature such as the bud energy balance is at equilibrium
+# Call the Ebalance function
+############################################################
+# inputs: inputs list defined in 1.0_create_database_Tbud.R
+#  inputs<-list(tair, in K
+#               patm, in kPa
+#               qair, in kg kg-1
+#               Sw, in W m-2
+#               Lw, in W m-2
+#               wind, in m s-1
+#               abs_sw) 0-1 unitless
+############################################################
+# outputs: Bud temperature in K
+############################################################
+
 Tbud<-function(inputs){
   
   # computes air relative humidity
@@ -8,17 +27,19 @@ Tbud<-function(inputs){
     return(eb)
   }
   
-  x1<-inputs$tair - 10
-  x2<-inputs$tair + 10
-  f1<-.f(inputs$tair - 10, inputs = inputs)
-  f2<-.f(inputs$tair + 10, inputs = inputs)
+  x1<-inputs$tair - 20
+  x2<-inputs$tair + 20
+  f1<-.f(inputs$tair - 20, inputs = inputs)
+  f2<-.f(inputs$tair + 20, inputs = inputs)
   
   if(sign(f1)==sign(f2)){
-    x1<-inputs$tair - 15
-    x2<-inputs$tair + 15
-    f1<-.f(inputs$tair - 15, inputs = inputs)
-    f2<-.f(inputs$tair + 15, inputs = inputs)
-  }
+    print("There is a problem with Tbud")
+    print("No root found, we set Tbud == tair")
+    print(paste0("Ebal tair-20°C = ",f1))
+    print(paste0("Ebal tair+20°C = ",f2))
+
+    return(inputs$tair) 
+  } else{
   
   results<-uniroot(
     f = .f, 
@@ -30,16 +51,7 @@ Tbud<-function(inputs){
     trace=10
   )
   return(results$root)
-  
-}
-
-qair2rh <- function(qair, temp, press = 1013.25){
-  es <-  6.112 * exp((17.67 * temp)/(temp + 243.5))
-  e <- qair * press / (0.378 * qair + 0.622)
-  rh <- e / es
-  rh[rh > 1] <- 1
-  rh[rh < 0] <- 0
-  return(rh)
+  }
 }
 
 
